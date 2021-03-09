f5bd85a8ed1b96000c7b4ef79fe637e3f862f69a
SohelRana682 <78408774+SohelRana682@users.noreply.github.com>
2021-03-09 18:45:48 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+
+
+    var typed = new Typed('.hate', {
+      strings: ["Hire me.", "i will degine your website."],
+      typeSpeed: 60,
+      backSpeed: 60,
+      loop:true
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
+
